06d9e93f14fb289c55d6a500caf8658652107d07
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-10-31 13:47:17 +0100

    Remove useless attributes

--- breadth-first-search/test.php
+++ breadth-first-search/test.php
@@ -4,31 +4,25 @@
     $array = array(
         'root' => array(
             'letter' => 'g',
-            'checked' => false,
             'children' => array(
                 array(
                     'letter' => 'c',
-                    'checked' => false,
                     'children' => array(
                         array(
                             'letter' => 'a',
-                            'checked' => false,
                             'children' => array()
                         )
                     )
                 ),
                 array(
                     'letter' => 'k',
-                    'checked' => false,
                     'children' => array(
                         array(
                             'letter' => 'h',
-                            'checked' => false,
                             'children' => array()
                         ),
                         array(
                             'letter' => 'm',
-                            'checked' => false,
                             'children' => array()
                         )
                     )
@@ -42,36 +36,29 @@
     $array = array(
         'root' => array(
             'letter' => 'g',
-            'checked' => false,
             'children' => array(
                 array(
                     'letter' => 'c',
-                    'checked' => false,
                     'children' => array(
                         array(
                             'letter' => 'a',
-                            'checked' => false,
                             'children' => array()
                         ),
                         array(
                             'letter' => 'b',
-                            'checked' => false,
                             'children' => array()
                         )
                     )
                 ),
                 array(
                     'letter' => 'k',
-                    'checked' => false,
                     'children' => array(
                         array(
                             'letter' => 'h',
-                            'checked' => false,
                             'children' => array()
                         ),
                         array(
                             'letter' => 'm',
-                            'checked' => false,
                             'children' => array()
                         )
                     )
@@ -85,52 +72,42 @@
     $array = array(
         'root' => array(
             'letter' => 'g',
-            'checked' => false,
             'children' => array(
                 array(
                     'letter' => 'c',
-                    'checked' => false,
                     'children' => array(
                         array(
                             'letter' => 'a',
-                            'checked' => false,
                             'children' => array()
                         ),
                         array(
                             'letter' => 'b',
-                            'checked' => false,
                             'children' => array()
                         )
                     )
                 ),
                 array(
                     'letter' => 'k',
-                    'checked' => false,
                     'children' => array(
                         array(
                             'letter' => 'h',
-                            'checked' => false,
                             'children' => array()
                         ),
                         array(
                             'letter' => 'm',
-                            'checked' => false,
                             'children' => array()
                         )
                     )
                 ),
                 array(
                     'letter' => 'l',
-                    'checked' => false,
                     'children' => array(
                         array(
                             'letter' => 'n',
-                            'checked' => false,
                             'children' => array()
                         ),
                         array(
                             'letter' => 'o',
-                            'checked' => false,
                             'children' => array()
                         )
                     )
